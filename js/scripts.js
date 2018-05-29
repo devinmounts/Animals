26ae8f83516f9832d82cafec1dd7ffb309ad6b55
Devin Mounts and Thad Donaghue <devin@>
2018-05-29 15:47:58 -0700

    add gray button and background.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("button#btn-dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("gray-background");
+  });
+
+  $("button#btn-dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("gray-background");
+  });
+
+  $("button#btn-dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("gray-background");
+  });
+});
